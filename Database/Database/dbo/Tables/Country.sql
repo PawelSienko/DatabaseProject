de8531176f8d41dfdb0ca091af3bc5564b95c091
Pawel <profesorklaus@gmail.com>
2016-09-18 19:23:30 +0200

    Init commit

---
+++ Database/Database/dbo/Tables/Country.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Country] (
+    [Id]        INT          NOT NULL,
+    [Iso]       CHAR (10)    NOT NULL,
+    [Name]      VARCHAR (50) NOT NULL,
+    [Nicename]  VARCHAR (50) NOT NULL,
+    [Iso3]      CHAR (3)     NULL,
+    [Numcode]   INT          NULL,
+    [Phonecode] INT          NOT NULL,
+    CONSTRAINT [PK_Country] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Database/Database/dbo/Tables/User.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[User] (
+    [Id]        BIGINT         NOT NULL,
+    [Name]      NVARCHAR (25)  NOT NULL,
+    [Surname]   NVARCHAR (25)  NOT NULL,
+    [Age]       SMALLINT       NULL,
+    [Email]     NVARCHAR (100) NOT NULL,
+    [Phone]     NVARCHAR (25)  NULL,
+    [Job]       NVARCHAR (50)  NULL,
+    [CountryId] TINYINT        NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
