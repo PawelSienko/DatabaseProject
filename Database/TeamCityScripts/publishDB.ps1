61e860b2a09a88bfbad7de09632ef97b9e5dcae4
Pawe� Sienko <psienko@objectivity.co.uk>
2016-10-03 16:34:23 +0200

    remove script

--- Database/TeamCityScripts/publishDB.ps1
+++
@@ -1,2 +0,0 @@
-Start-Process -FilePath "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\130\sqlpackage.exe"  -ArgumentList "/Action:Publish /Sourcefile:C:\TeamCity\buildAgent\work\1.DataBank\Database\Database\bin\Release\DataBank.dacpac /TargetDatabaseName:DataBank /TargetServerName:13.95.19.4 /TargetUser:Ext /TargetPassword:q01654832- /p:CreateNewDatabase=$True"
-
