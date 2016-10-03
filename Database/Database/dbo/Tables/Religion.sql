44cd1f912e9217b7b19885e51607fae73c97b1ed
Pawel <profesorklaus@gmail.com>
2016-10-03 21:34:40 +0200

    Changed the order of columns.

--- Database/Database/dbo/Tables/Religion.sql
+++ Database/Database/dbo/Tables/Religion.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[Religion] (
+	[ReligionId]  INT            IDENTITY (1, 1) NOT NULL,
     [Name]        NVARCHAR (150) NOT NULL,
     [Description] TEXT           NOT NULL,
     [Gods]        NVARCHAR (100) NOT NULL,
     [Afterlife]   NVARCHAR (100) NOT NULL,
     [HolyText]    NVARCHAR (100) NOT NULL,
-    [ReligionId]  INT            IDENTITY (1, 1) NOT NULL,
     CONSTRAINT [PK_Religion] PRIMARY KEY CLUSTERED ([ReligionId] ASC)
 );
 
