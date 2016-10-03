87ad825e828b3d5a6974110f82f000cc3c2e0b38
Pawe� Sienko <psienko@objectivity.co.uk>
2016-10-03 16:00:21 +0200

    update

--- Database/TeamCityScripts/publishDB.ps1
+++ Database/TeamCityScripts/publishDB.ps1
@@ -2,8 +2,7 @@ param([string] $dacpackPath,
 [string] $targetDatabaseName,
 [string] $targetServerName,
 [string] $targetUser,
-[string] $targetPassword,
-[bool] $createNewDB)
+[string] $targetPassword)
 {
     Start-Process -FilePath "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\sqlpackage.exe"  -ArgumentList "/Action:Publish /Sourcefile:C:\TeamCity\buildAgent\work\1.DataBank\Database\Database\bin\Release\DataBank.dacpac /TargetDatabaseName:DataBank /TargetServerName:13.95.19.4 /TargetUser:Ext /TargetPassword:q01654832- /p:CreateNewDatabase=$True"
 }
