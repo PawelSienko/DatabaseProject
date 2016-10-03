36c61f9da6223807f9129fae9e151f317791ce4c
Pawe� Sienko <psienko@objectivity.co.uk>
2016-10-03 14:58:11 +0200

    udpate

--- Database/TeamCityScripts/publishDB.ps1
+++ Database/TeamCityScripts/publishDB.ps1
@@ -5,5 +5,5 @@ param([string] $dacpackPath,
 [string] $targetPassword,
 [bool] $createNewDB)
 {
-    & "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\sqlpackage.exe" /Action:Publish /$dacpackPath /$targetDatabaseName /$targetServerName /$targetUser /$targetPassword /$createNewDB
+    Start-Process -FilePath "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\sqlpackage.exe" -ArgumentList "/Action:Publish /$dacpackPath /$targetDatabaseName /$targetServerName /$targetUser /$targetPassword /$createNewDB"
 }
