f80a93f9ffc30adde850ab2ab0b33826508f2b35
Pawel <profesorklaus@gmail.com>
2016-10-03 21:57:56 +0200

    Added missing files.

---
+++ Database/Database/Scripts/Inserts/InsertUSStates.sql
@@ -0,0 +1,109 @@
+﻿USE [DataBank]
+GO
+SET IDENTITY_INSERT [dbo].[USState] ON 
+
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'DC', N'District of Columbia', 1)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'AL', N'Alabama', 2)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'AK', N'Alaska', 3)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'AZ', N'Arizona', 4)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'AR', N'Arkansas', 5)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'CA', N'California', 6)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'CO', N'Colorado', 7)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'CT', N'Connecticut', 8)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'DE', N'Delaware', 9)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'FL', N'Florida', 10)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'GA', N'Georgia', 11)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'HI', N'Hawaii', 12)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'ID', N'Idaho', 13)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'IL', N'Illinois', 14)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'IN', N'Indiana', 15)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'IA', N'Iowa', 16)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'KS', N'Kansas', 17)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'KY', N'Kentucky', 18)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'LA', N'Louisiana', 19)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'ME', N'Maine', 20)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'MD', N'Maryland', 21)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'MA', N'Massachusetts', 22)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'MI', N'Michigan', 23)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'MN', N'Minnesota', 24)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'MS', N'Mississippi', 25)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'MO', N'Missouri', 26)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'MT', N'Montana', 27)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'NE', N'Nebraska', 28)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'NV', N'Nevada', 29)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'NH', N'New Hampshire', 30)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'NJ', N'New Jersey', 31)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'NM', N'New Mexico', 32)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'NY', N'New York', 33)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'NC', N'North Carolina', 34)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'ND', N'North Dakota', 35)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'OH', N'Ohio', 36)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'OK', N'Oklahoma', 37)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'OR', N'Oregon', 38)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'PA', N'Pennsylvania', 39)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'RI', N'Rhode Island', 40)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'SC', N'South Carolina', 41)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'SD', N'South Dakota', 42)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'TN', N'Tennessee', 43)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'TX', N'Texas', 44)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'UT', N'Utah', 45)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'VT', N'Vermont', 46)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'VA', N'Virginia', 47)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'WA', N'Washington', 48)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'WV', N'West Virginia', 49)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'WI', N'Wisconsin', 50)
+GO
+INSERT [dbo].[USState] ([Code], [Name], [USStateId]) VALUES (N'WY', N'Wyoming', 51)
+GO
+SET IDENTITY_INSERT [dbo].[USState] OFF
+GO
---
+++ Database/Database/dbo/Tables/USState.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[USState] (
+	[USStateId] INT          IDENTITY (1, 1) NOT NULL,
+    [Code]      CHAR (10)    NOT NULL,
+    [Name]      VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_USState] PRIMARY KEY CLUSTERED ([USStateId] ASC)
+);
+
